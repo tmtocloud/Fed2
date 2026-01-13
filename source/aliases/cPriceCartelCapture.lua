dedb9b9b52aa15e549d767dbebf2549f31d30c52
tmtocloud <tmtocloud@gmail.com>
2026-01-13 01:16:36 -0500

    Delete source/aliases directory

--- source/aliases/cPriceCartelCapture.lua
+++
@@ -1,7 +0,0 @@
--- This is an alias with regex: ^c price (\w+) cartel$
-
-FEDUI.cargo.currentCommodity = matches[2]
-FEDUI.cargo.data = {}
-FEDUI.cargo.lastLineWasPrice = false
-FEDUI.cargoWindow:clear()
-send("c price " .. matches[2] .. " cartel", false)
